bbf49c530340bb105efad4de3877ce5460d81137
choihelenh <helen.choi3267@gmail.com>
2025-05-05 21:44:53 -0400

    fixes to lag

--- forecast_code/R/generate_secchi_arimax.R
+++ forecast_code/R/generate_secchi_arimax.R
@@ -15,16 +15,16 @@ generate_secchi_arimax <- function(forecast_date,
   fcre_smoothed <- interpolate_secchi(fcre_combined)
 
   # 4. ----- Build regressors -------------------------------------------
-  ## (your lag‑23 rain + noon means code here, but using `historical_weather`
+  ## (your lag‑6 rain + lag-15 air temp+ noon means code here, but using `historical_weather`
   ##  and `future_weather` variables)
   # print(fcre_smoothed)
   smoothed_data <- fcre_smoothed %>%
     select(datetime, Secchi_m, Rain_mm_sum, AirTemp_C_mean, Secchi_m_smoothed) %>%
     mutate(
-      Rain_lag23 = lag(Rain_mm_sum, 23),
-      AirTemp_lag0 = AirTemp_C_mean
+      Rain_lag6 = lag(Rain_mm_sum, 6),
+      AirTemp_lag15 = lag(AirTemp_C_mean, 15)
     ) %>%
-    filter(!is.na(Secchi_m_smoothed), !is.na(Rain_lag23), !is.na(AirTemp_lag0))
+    filter(!is.na(Secchi_m_smoothed), !is.na(Rain_lag6), !is.na(AirTemp_lag15))
   # print(smoothed_data)
 
  # get future data
@@ -69,19 +69,20 @@ generate_secchi_arimax <- function(forecast_date,
     select(datetime, Rain_mm_sum, AirTemp_C_mean) %>%
     bind_rows(noon_future_regs) %>%
     arrange(datetime) %>%
-    mutate(Rain_lag23 = lag(Rain_mm_sum, 23))
+    mutate(Rain_lag6 = lag(Rain_mm_sum, 6))  %>%
+    mutate(AirTemp_lag15 = lag(AirTemp_C_mean, 15))
 
   future_predictors <- all_meteo %>%
     mutate(date = as.Date(datetime)) %>%
     filter(date %in% as.Date(noon_future_regs$datetime)) %>%
-    filter(!is.na(Rain_lag23), !is.na(AirTemp_C_mean)) %>%
-    select(-date)  # Optional: drop helper column
+    filter(!is.na(Rain_lag6), !is.na(AirTemp_lag15))
 
 
   secchi_data <- fcre_smoothed %>%
     select(datetime, Secchi_m, Rain_mm_sum, AirTemp_C_mean, Secchi_m_smoothed) %>%
-    mutate(Rain_lag23 = lag(Rain_mm_sum, 23)) %>%
-    filter(!is.na(Secchi_m_smoothed), !is.na(Rain_lag23), !is.na(AirTemp_C_mean))
+    mutate(Rain_lag6 = lag(Rain_mm_sum, 6)) %>%
+    mutate(AirTemp_lag15 = lag(AirTemp_C_mean, 15))%>%
+    filter(!is.na(Secchi_m_smoothed), !is.na(Rain_lag6), !is.na(AirTemp_lag15))
   # print(secchi_data)
   # print(tail(secchi_data))
   # na_counts_base <- colSums(is.na(secchi_data))
@@ -94,10 +95,10 @@ generate_secchi_arimax <- function(forecast_date,
   train <- secchi_data
 
   ts.train <- ts(train$Secchi_m_smoothed, frequency = 365)
-  xreg_train <- as.matrix(train %>% select(Rain_lag23, AirTemp_C_mean))
+  xreg_train <- as.matrix(train %>% select(Rain_lag6, AirTemp_lag15))
 
   # ---- Step 7: Forecast future Secchi ----
-  future_xreg <- as.matrix(future_predictors %>% select(Rain_lag23, AirTemp_C_mean))
+  future_xreg <- as.matrix(future_predictors %>% select(Rain_lag6, AirTemp_lag15))
 
   fit <- stlm(ts.train, s.window = "periodic", method = "arima", xreg = xreg_train)
   fc <- forecast(fit, h = nrow(future_predictors), newxreg = future_xreg)
@@ -117,7 +118,7 @@ generate_secchi_arimax <- function(forecast_date,
     geom_point(aes(y = forecast), color = "#0570b0", linewidth = 2) +
     labs(
       title = "30-Day Forecast of Secchi Depth",
-      subtitle = "Model: Rain (lag 23) + Air Temp",
+      subtitle = "Model: Rain (lag 6) + Air Temp (lag 15)",
       x = "Date", y = "Secchi Depth (m)"
     ) +
     theme_minimal(base_size = 14)
